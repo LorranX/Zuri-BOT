19e51d93592c9027bd2fb0ba151760afbd8a7033
Lorran Avelar <89737304+LorranX@users.noreply.github.com>
2022-12-06 08:40:34 -0300

    Add support to new platforms and new functions
    now whatsapp web and ios users can use the bot while ephemeral messages are on

--- start.js
+++ start.js
@@ -76,9 +76,10 @@ async function OpenConn() {
 
   l.ev.on('messages.upsert', connection => {
     mek = connection.messages[0];
-    if (mek.key.fromMe) return;
-    if (connection.type != 'notify') return;
-    if (mek.key.remoteJid === 'status@broadcast') return;
+    if (!mek.message) return
+    mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+    if (mek.key && mek.key.remoteJid === 'status@broadcast') return
+    if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16 && mek.key.fromMe) return
     mek = smsg(l, mek, store)
     require('./index')(l, mek);
   });
@@ -95,7 +96,7 @@ async function OpenConn() {
     } else return jid
   }
 
-  /** quick reply a message
+  /** Quick reply a message
     *
     * @param {*} from
     * @param {*} t
@@ -103,11 +104,36 @@ async function OpenConn() {
     * @returns
     */
 
-  l.reply = (from, t, q) => {
-    l.sendMessage(from, { text: t }, { quoted: q })
+  l.reply = (jid, t, q) => {
+    l.sendMessage(jid, { text: t }, { quoted: q })
   }
 
+  /** React to a messages 
+     * 
+     * @param {*} jid 
+     * @param {*} reaction
+     * @param {*} msg
+     * @returns 
+     */
+
+  l.react = (jid, reaction, msg) => {
+   reactionMessage = {
+       react: {
+           text: reaction,
+           key: {
+               remoteJid: msg.chat,
+               fromMe: false,
+               id: msg.id,
+               participant: msg.sender
+           }
+       }
+   }
+   l.sendMessage(jid, reactionMessage)
+ 
+ }
 }
+
+
 
 OpenConn(), (err) => console.log("[ Connection Error ]", color(String(err), 'red'));
   
